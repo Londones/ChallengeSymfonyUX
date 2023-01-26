32429ccaac5c959eaebc638a14e9456614ba21d5
aliciasaci <lyciiasci@gmail.com>
2023-01-26 07:54:27 +0100

    register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['email'], message: "this emails already exists")]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
