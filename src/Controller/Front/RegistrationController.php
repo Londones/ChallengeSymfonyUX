3b830d99cea9a4e0a797ff701bc14b892d1114b4
aliciasaci <lyciiasci@gmail.com>
2023-01-29 19:00:19 +0100

    verify mail & reset-password

--- src/Controller/Front/RegistrationController.php
+++ src/Controller/Front/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\LoginAuthenticator;
@@ -12,11 +13,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(MailerInterface $mailer, Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -33,7 +36,17 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
+
+            //*envoyer un email de confirmation 
+            $email = (new TemplatedEmail())
+                ->from(new Address('sacialicia2@gmail.com', 'noreply'))
+                ->to(new Address($user->getEmail(), $user->getName()))
+                ->subject('Confirmation de votre adresse mail')
+                ->htmlTemplate('emails/confirme_email.html.twig')
+                ->context([
+                    'user' => $user
+                ]);
+            $mailer->send($email);
 
             return $userAuthenticator->authenticateUser(
                 $user,
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -19,14 +19,14 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('email')
             ->add('name')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'You should agree to our terms.',
+            //         ]),
+            //     ],
+            // ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
