17fd165e4836a52cec98ed8908b619f647b91e13
Redeltaz <lucascampistron95@gmail.com>
2023-01-29 22:31:57 +0100

    feat: add linter + makefile scripts

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[UniqueEntity(fields: ['email'], message: "this emails already exists")]
+#[UniqueEntity(fields: ['email'], message: 'this emails already exists')]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -41,7 +41,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->id;
     }
-    
+
     // public function setId(string $id): self
     // {
     //     $this->id = $id;
