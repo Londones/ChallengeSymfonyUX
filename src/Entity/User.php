ef052ac41be63b30a520044ed141c0b71a8d2fbf
aliciasaci <lyciiasci@gmail.com>
2023-02-18 21:35:26 +0100

    profil images

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use App\Entity\Traits\TimestampableTrait;
-use Symfony\Component\Validator\Constraints\Length;
+// use Symfony\Component\Validator\Constraints\Length;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 
@@ -24,7 +24,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    private ?int $id = null;
+    private ?int $id = null;                              
 
     #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
