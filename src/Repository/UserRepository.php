ed7a4f762691b8513fd592c8db66b842d4fa1763
Redeltaz <lucascampistron95@gmail.com>
2023-02-20 00:42:44 +0100

    feat(swipe): wip

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -93,12 +93,8 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
             ->join('u.category', 'c')
             ->where('c.id IN (:categoriesId)')
             ->andWhere('u.id != :userId')
-            ->andWhere('u.id NOT IN (:swippedId)')
             ->setParameter('categoriesId', $categoriesId)
-            ->setParameter('swippedId', $swippedId)
-            ->setParameter('userId', $user->getId())
-            ->orderBy('u.createdAt', 'ASC')
-            ->setMaxResults(1);
+            ->setParameter('userId', $user->getId());
         $query = $qb->getQuery();
         $result = $query->getResult();
 
