eb24172d0fbec6ededd743ee5ce83b6943f28ea0
Redeltaz <lucascampistron95@gmail.com>
2023-02-23 14:19:36 +0100

    fix(profil): fix mistakes

--- src/Controller/Front/ProfileController.php
+++ src/Controller/Front/ProfileController.php
@@ -2,72 +2,72 @@
 
 namespace App\Controller\Front;
 
+<<<<<<< HEAD
 use App\Entity\User;
 use App\Entity\Deal;
 use App\Form\ProfileType;
 use App\Repository\DealRepository;
 use App\Repository\ItemsRepository;
+=======
+use App\Form\ProfileType;
+>>>>>>> 331b55a... fix(profil): fix mistakes
 use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/profile')]
+#[Route('/profil', name: 'profil_')]
 #[UniqueEntity(fields: ['email'], message: "Le user existe déjà")]
 class ProfileController extends AbstractController
 {
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-    public function show(User $user, UserRepository $userRepository): Response
+    #[Route('/me', name: 'me', methods: ['GET'])]
+    public function show(): Response
     {
         $user = $this->getUser();
-        if ($user) {
+        if($user){
             $itemsOfUser = $user->getItems();
         } else {
             return $this->redirectToRoute('front_app_login');
         }
-        return $this->render('user/show.html.twig', [
+        return $this->render('front/profil/show.html.twig', [
             'user' => $user,
             'items' => $itemsOfUser,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    #[Route('/me/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, UserRepository $userRepository): Response
     {
+        $user = $this->getUser();
+
         $form = $this->createForm(ProfileType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->save($user, true);
 
-            return $this->redirectToRoute('front_app_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('front_profil_me',[], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('user/edit.html.twig', [
+        return $this->renderForm('front/profil/edit.html.twig', [
             'user' => $user,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    #[Route('/me', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, UserRepository $userRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
-            $userRepository->remove($user, true);
+        if ($this->isCsrfTokenValid('delete'.$this->getUser()->getId(), $request->request->get('_token'))) {
+            $session = new Session();
+            $session->invalidate();
+            $userRepository->remove($this->getUser(), true);
         }
 
-        return $this->redirectToRoute('front_app_user_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('front_app_login');
     }
 
     #[Route('/public/{id}/', name: 'app_user_show_public', methods: ['GET'])]
